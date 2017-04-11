f8f25257bcfdf3a114b5a06e632f1d86024351b2
Adam Bananka <xbananka@nymfe86.fi.muni.cz>
2017-04-11 15:07:23 +0200

    mission web update version1 added

--- security_agency-webapp/src/main/java/cz/muni/fi/pv168/web/MissionServlet.java
+++ security_agency-webapp/src/main/java/cz/muni/fi/pv168/web/MissionServlet.java
@@ -1,5 +1,6 @@
 package cz.muni.fi.pv168.web;
 
+import cz.muni.fi.pv168.app.common.IllegalEntityException;
 import cz.muni.fi.pv168.app.mission.*;
 
 import javax.servlet.ServletException;
@@ -31,40 +32,87 @@ public class MissionServlet extends HttpServlet{
         String action = request.getPathInfo();
         switch (action) {
             case "/add":
-                String name = request.getParameter("name");
-                String rank = request.getParameter("rank");
-                if (name == null || name.length() == 0 || rank == null || rank.length() == 0) {
-                    request.setAttribute("Error", "All fields must be filled!");
-                    showMissionsList(request, response);
-                    return;
-                }
-                try {
-                    Mission mission = new Mission();
-                    mission.setName(name);
-                    mission.setStatus(MissionStatus.NOT_ASSIGNED);
-                    mission.setRequiredRank(Integer.parseInt(rank));
-                    getMissionManager().createMission(mission);
-                    response.sendRedirect(request.getContextPath()+URL_MAPPING);
-                    return;
-                } catch (Exception e) {
-                    response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
-                    return;
-                }
+                add(request, response);
+                break;
             case "/delete":
-                try {
-                    Long id = Long.valueOf(request.getParameter("id"));
-                    getMissionManager().deleteMission(getMissionManager().findMission(id));
-                    response.sendRedirect(request.getContextPath()+URL_MAPPING);
-                    return;
-                } catch (Exception e) {
-                    response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
-                    return;
-                }
+                delete(request, response);
+                break;
+            case "/initUpdate":
+                initUpdate(request, response);
+                break;
             case "/update":
-                //TODO
-                return;
+                update(request, response);
+                break;
             default:
                 response.sendError(HttpServletResponse.SC_NOT_FOUND, "Unknown action " + action);
+        }
+    }
+
+    private void add(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String rank = request.getParameter("rank");
+        if (name == null || name.length() == 0 || rank == null || rank.length() == 0) {
+            request.setAttribute("Error", "All fields must be filled!");
+            showMissionsList(request, response);
+            return;
+        }
+        try {
+            Mission mission = new Mission();
+            mission.setName(name);
+            mission.setStatus(MissionStatus.NOT_ASSIGNED);
+            mission.setRequiredRank(Integer.parseInt(rank));
+            getMissionManager().createMission(mission);
+            response.sendRedirect(request.getContextPath()+URL_MAPPING);
+        } catch (Exception e) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+
+    private void delete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            Long id = Long.valueOf(request.getParameter("id"));
+            getMissionManager().deleteMission(getMissionManager().findMission(id));
+            response.sendRedirect(request.getContextPath()+URL_MAPPING);
+        } catch (Exception e) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+
+    private void initUpdate(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("update", "Mission updating.");
+        showMissionsList(request, response);
+        response.sendRedirect(request.getContextPath()+URL_MAPPING);
+    }
+
+    private void update(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String rank = request.getParameter("rank");
+        String status = request.getParameter("status");
+        if (name == null || name.length() == 0 || rank == null || rank.length() == 0
+                || status == null || status.length() == 0) {
+            request.setAttribute("Error", "All fields must be filled!");
+            showMissionsList(request, response);
+            return;
+        }
+        try {
+            Mission mission = getMissionManager().findMission(Long.valueOf(request.getParameter("id")));
+            mission.setName(name);
+            switch (Integer.parseInt(status)) {
+                case 0: mission.setStatus(MissionStatus.NOT_ASSIGNED);
+                    break;
+                case 1: mission.setStatus(MissionStatus.IN_PROGRESS);
+                    break;
+                case 2: mission.setStatus(MissionStatus.ACCOMPLISHED);
+                    break;
+                case 3: mission.setStatus(MissionStatus.FAILED);
+                    break;
+                default: throw new IllegalEntityException("wrong status");
+            }
+            mission.setRequiredRank(Integer.parseInt(rank));
+            getMissionManager().updateMission(mission);
+            response.sendRedirect(request.getContextPath()+URL_MAPPING);
+        } catch (Exception e) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
         }
     }
 
--- security_agency-webapp/target/security_agency-webapp-1.0/list.jsp
+++ security_agency-webapp/target/security_agency-webapp-1.0/list.jsp
@@ -16,30 +16,60 @@
             <td><m:out value="${mission.name}"/></td>
             <td><m:out value="${mission.status}"/></td>
             <td><m:out value="${mission.requiredRank}"/></td>
+            <td><form method="post" action="${pageContext.request.contextPath}/missions/initUpdate?id=${mission.id}"
+                      style="margin-bottom: 0;"><input type="submit" value="Update"></form> </td>
             <td><form method="post" action="${pageContext.request.contextPath}/missions/delete?id=${mission.id}"
                       style="margin-bottom: 0;"><input type="submit" value="Delete"></form></td>
         </tr>
     </m:forEach>
 </table>
 
-<h2>Insert mission</h2>
-<m:if test="${not empty chyba}">
-    <div style="border: solid 1px red; background-color: yellow; padding: 10px">
-        <m:out value="${chyba}"/>
-    </div>
+<m:if test="${empty update}">
+    <h2>Insert mission</h2>
+    <m:if test="${not empty error}">
+        <div style="border: solid 1px red; background-color: yellow; padding: 10px">
+            <m:out value="${error}"/>
+        </div>
+    </m:if>
+    <form action="${pageContext.request.contextPath}/missions/add" method="post">
+        <table>
+            <tr>
+                <th>Name:</th>
+                <td><input type="text" name="name" value="<m:out value='${param.name}'/>"/></td>
+            </tr>
+            <tr>
+                <th>Required rank:</th>
+                <td><input type="number" name="rank" value="<m:out value='${param.rank}'/>"/></td>
+            </tr>
+        </table>
+        <input type="Submit" value="Insert" />
+    </form>
 </m:if>
-<form action="${pageContext.request.contextPath}/missions/add" method="post">
-    <table>
-        <tr>
-            <th>Name:</th>
-            <td><input type="text" name="name" value="<m:out value='${param.name}'/>"/></td>
-        </tr>
-        <tr>
-            <th>Required rank:</th>
-            <td><input type="number" name="rank" value="<m:out value='${param.rank}'/>"/></td>
-        </tr>
-    </table>
-    <input type="Submit" value="Insert" />
-</form>
+
+<m:if test="${not empty update}">
+    <h2>Update mission</h2>
+    <m:if test="${not empty error}">
+        <div style="border: solid 1px red; background-color: yellow; padding: 10px">
+            <m:out value="${error}"/>
+        </div>
+    </m:if>
+    <form action="${pageContext.request.contextPath}/missions/update" method="post">
+        <table>
+            <tr>
+                <th>Name:</th>
+                <td><input type="text" name="name" value="<m:out value='${param.name}'/>"/></td>
+            </tr>
+            <tr>
+                <th>Status: (0=NOT_ASSIGNED, 1=IN_PROGRESS, 2=ACCOMPLISHED, 3=FAILED)</th>
+                <td><input type="number" name="status" value="<m:out value='${param.status}'/>"/></td>
+            </tr>
+            <tr>
+                <th>Required rank:</th>
+                <td><input type="number" name="rank" value="<m:out value='${param.rank}'/>"/></td>
+            </tr>
+        </table>
+        <input type="Submit" value="Update" />
+    </form>
+</m:if>
 </body>
 </html>
